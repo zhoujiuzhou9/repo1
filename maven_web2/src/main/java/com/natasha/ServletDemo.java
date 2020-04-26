46f64bda5bbc51669b428dab1c8c9c063181cdb6
andre <jiuzhou.zhou9@gmail.com>
2020-04-26 14:50:01 +0900

    1

---
+++ maven_web2/src/main/java/com/natasha/ServletDemo.java
@@ -0,0 +1,17 @@
+package com.natasha;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletDemo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("hello.jsp").forward(request,response);
+    }
+}
---
+++ maven_web2/src/main/webapp/hello.jsp
@@ -0,0 +1,10 @@
+<%--User: andreDate: 2020/04/26Time: 0:17--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    hello jsp
+</body>
+</html>
